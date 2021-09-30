ddfa0a8f7fa37cdb683e9681af53883f022406b8
Pasquale-Raso <padqualeraso@gmail.com>
2021-09-30 15:31:52 +0200

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>HOME PAGE</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat saepe inventore sapiente ratione cum quasi asperiores impedit! Similique quaerat molestias assumenda sunt harum ipsum consectetur? Eum quia animi quo consequatur.</p>
+    
+</body>
+</html>
